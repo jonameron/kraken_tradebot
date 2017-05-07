d4165d42441b1b401c7e5f02d28964a269e80b76
jonameron <jonathan.staude@gmail.com>
2017-05-07 19:32:34 +0200

    divide code into smaller services

--- index.R
+++ index.R
@@ -21,58 +21,8 @@ ifelse(x=="",
           return(c(x,y))
 }
 a <- ask_keys()
-
-
-#get last download time
-r <- read.csv(file = "./Data/download_times.csv",sep = ";",header = FALSE,col.names = c("id","last"))
-snce <- as.character(tail(r$last,n = 1))
-#tradeSet <- kraken_PublicFn("Trades?pair=XETHZEUR&since=1494094742768336630")
-#tradeSet <- kraken_PublicFn(paste("Trades?pair=XETHZEUR&since=",snce,sep=""))
-tradeSet <- kraken_PublicFn("Trades?pair=XETHZEUR") #backup
-trades <- as.data.frame(do.call(rbind,tradeSet$XETHZEUR))
-
-colnames(trades) <-c("FX","volume","timestamp","b/s","l/m","NnA")
-class(trades$FX) <- "numeric"
-class(trades$volume) <- "numeric"
-class(trades$timestamp) <- "numeric"
-class(trades$`b/s`)="character"
-class(trades$`l/m`)="character"
-trades <- subset(trades,select=-6)
-
-#times ...
-last_timestamp <- as.numeric(trades$timestamp[length(trades$timestamp)])
-first_timestamp <- as.numeric(trades$timestamp[1])
-period <- function(t="s"){
-  p <- last_timestamp-first_timestamp
-  switch(t,
-        h = p/3600,
-        m = p/60,
-        s = p)
-      }
-
-#plotting ...
-linear_model <- lm(FX ~ timestamp, data = trades)
-trades["lm"]<- predict(linear_model)
-slope <- linear_model$coefficients[2]
-ifelse(test = slope>0,yes = print("... positive slope..."),no=print("... negative slope..."))
-
-p <- plot_ly( x = (trades$timestamp-first_timestamp), y = trades$FX, name = 'FX', type = 'scatter', mode = 'lines')%>%
-  add_trace(x=(trades$timestamp-first_timestamp),y=trades$lm,name =paste('regr , s=',format(linear_model$coefficients[2],digits=3),sep=""),type ='scatter',evaluation = FALSE)
-
-
-#writing data to file
-write.table(x = trades,file="./Data/trades_history.csv",append = TRUE,col.names = FALSE,sep = ";",dec = ",")
-
-#document last
-#document last query
-prev_timestamp <- as.numeric(trades$timestamp[length(trades$timestamp)])
-write.table(tradeSet$last,file = "./Data/download_times.csv",append = TRUE,col.names = FALSE,sep = ";",dec = ",")
-
-#returns slope of tail model by fraction, 5 for example for 5th-last quantile
-tailslope <- function(quantile) #enter 5 to see fifth quantile
-      {tailNo <- as.integer(length(trades$FX)/quantile)
-      tail <- tail(x=trades,n=tailNo)
-      model <- lm(tail$FX ~ tail$timestamp)
-      return(model$coefficients[2])
-      }
+source("trades.R")
+source("plots.R")
 p
+
+
